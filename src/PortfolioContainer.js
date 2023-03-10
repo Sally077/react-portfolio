bac0e920ed6711d16cf345edec21b7f79f818b6c
Sally077 <sallyhedges77@gmail.com>
2023-03-10 19:19:17 +0000

    Added code for navigation

---
+++ src/PortfolioContainer.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+class Portfolio extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === "Home") {
+      return <Home />;
+    } else if (this.state.currentPage === "Projects") {
+      return <Projects />;
+    } else if (this.state.currentPage === "Contact") {
+      return <Contact />;
+    } else {
+      return <Contact />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <NavTabs
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
