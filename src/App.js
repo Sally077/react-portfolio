537ee8c085a98427f3e30002fffca502aed85935
Sally077 <sallyhedges77@gmail.com>
2023-03-11 08:27:24 +0000

    Installed react-route-dom and modified code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import NavTabs from "./Components/NavTabs";
+import Home from "./Components/Home";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <Router>
+        <div>
+          <NavTabs />
+          {/* Wrap Route elements in a Routes component */}
+          <Routes>
+            {/* Define routes using the Route component to render different page components at different paths */}
+            {/* Define a default route that will render the Home component */}
+            <Route path="/" element={<Home />} />
+            <Route path="Projects" element={<Projects />} />
+            {/* <Route path="Contact" element={<Contact />} /> */}
+            {/* Define a route that will have descendant routes */}
+            <Route path="contact/*" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
+  
+  export default App;
+  
