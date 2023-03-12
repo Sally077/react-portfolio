719c9347715812b2377b6c4d83b249b4ca2403f3
Sally077 <sallyhedges77@gmail.com>
2023-03-12 20:03:54 +0000

    installed fontawesome and added hamburger menu

--- src/Components/NavTab.js
+++ src/Components/NavTab.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../avatar.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons"; // <-- import styles to be used
+
+{/* <FontAwesomeIcon icon={solid('faBars')} /> */}
+
+
+
 
 function NavTabs() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <div className="container">
   <a classNameName="navbar-brand" href="#"><img className="logo" src={logo} alt="Avatar logo..."></img></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span classNameName="navbar-toggler-icon"></span>
+  <FontAwesomeIcon icon={faBars}/>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -16,9 +24,13 @@ function NavTabs() {
         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+        <a className="nav-link" href="#">Projects</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact</a>
       </li>
     </ul>
+    </div>
   </div>
 </nav>
   
